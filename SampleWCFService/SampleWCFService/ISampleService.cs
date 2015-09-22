612266717277f19e68ba033490407cd2fba08732
aande <ashoknet.ande@gmail.com>
2015-09-22 18:08:12 +0530

    Wcf service project added

---
+++ SampleWCFService/SampleWCFService/ISampleService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SampleWCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface ISampleService
+    {
+        [OperationContract]
+        string GetMessage(string message);
+    }
+
+}
